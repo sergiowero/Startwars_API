7beec84bc345767372056b816af8d3ba3519fe2c
Sergio Sanchez <sergioj.sanchezr@gmail.com>
2017-11-08 18:24:53 -0600

    Initial Commit

---
+++ Startwars_API/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Startwars_API
+//
+//  Created by Virtual Box on 11/8/17.
+//  Copyright © 2017 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Startwars_API/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Startwars_API
+//
+//  Created by Virtual Box on 11/8/17.
+//  Copyright © 2017 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
