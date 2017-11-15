e2b9bba1070ab5ca857ab396afa1183c1de93650
Sergio Sanchez <sergioj.sanchezr@gmail.com>
2017-11-14 22:43:59 -0600

    Added cell and code to load people in the table

---
+++ Startwars_API/Cells/SWPersonCell.h
@@ -0,0 +1,14 @@
+//
+//  SWPeopleCell.h
+//  Startwars_API
+//
+//  Created by Virtual Box on 11/14/17.
+//  Copyright © 2017 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWPersonCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *labelName;
+
+@end
--- Startwars_API/Controllers/Home.m
+++ Startwars_API/Controllers/Home.m
@@ -9,16 +9,18 @@
 #import "Home.h"
 #import "WebServices.h"
 #import "Constants.h"
+#import "SWPersonCell.h"
 
 @interface Home ()
-
+@property NSMutableArray *people;
 @end
 
 @implementation Home
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _people = [[NSMutableArray alloc] init];
 }
 
 
@@ -30,4 +32,40 @@
 - (IBAction)btnRefreshPressed:(id)sender {
 }
 
+-(void) refreshPeople {
+    
+}
+
+#pragma mark - Table
+
+- (NSInteger)numberOfSectionTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.people count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return 50.5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    SWPersonCell *cell = (SWPersonCell *)[tableView dequeueReusableCellWithIdentifier:@"SWPeopleCell"];
+    
+    if (cell == nil) {
+        [tableView registerNib:[UINib nibWithNibName:@"SWPeopleCell" bundle:nil] forCellReuseIdentifier:@"SWPeopleCell"];
+        cell = [tableView dequeueReusableCellWithIdentifier:@"SWPeopleCell"];
+    }
+    
+    //Fill cell with info from arrays
+    cell.labelName.text = [[self.people objectAtIndex:[indexPath row]] name];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self performSegueWithIdentifier:@"PeopleTableToDetail" sender:self];
+}
 @end
